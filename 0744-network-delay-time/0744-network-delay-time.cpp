c7d3be752ba4c58810f1881f1f5d47ce077018db
Vishal Singh <156567984+sangeetanandanvishal04@users.noreply.github.com>
2025-06-17 14:38:12 +0530

    Time: 107 ms (10.79%), Space: 47.9 MB (33.81%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int, int>>> adj(n+1);
+        for(auto time: times){
+            int u = time[0];
+            int v = time[1];
+            int t = time[2];
+
+            adj[u].push_back({v, t});
+        }
+        
         vector<int> dist(n+1, 1e9);
         dist[k] = 0;
         
-        for(int i=1; i<n; i++){
-            for(auto edge: times){
-                int u = edge[0];
-                int v = edge[1];
-                int edW = edge[2];
-                
-                if(dist[u] != 1e8 && dist[u] + edW < dist[v]){
-                    dist[v] = dist[u] + edW;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, k});
+
+        while(!pq.empty()){
+            int time = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            for(auto it: adj[node]){
+                int adjNode = it.first;
+                int edW = it.second;
+                if(time + edW < dist[adjNode]){
+                    dist[adjNode] = time + edW;
+                    pq.push({time+edW, adjNode});
                 }
             }
         }
