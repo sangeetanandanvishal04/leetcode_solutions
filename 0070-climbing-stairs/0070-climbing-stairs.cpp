4f7684a001b6940939fcdbb9049e5313139650c6
Vishal Singh <156567984+sangeetanandanvishal04@users.noreply.github.com>
2025-06-24 13:20:23 +0530

    Time: 0 ms (100%), Space: 8.6 MB (17.78%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -13,7 +13,16 @@ private:
     }     
 public:
     int climbStairs(int n) {
-        vector<int> memo(n+1, -1);
-        return Solve(n, memo);
+        //vector<int> memo(n+1, -1);
+        //return Solve(n, memo);
+
+        vector<int> dp(n+1, 0);
+        dp[0] = dp[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 };
