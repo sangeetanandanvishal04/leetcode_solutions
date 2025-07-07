d2c0af500599a2b5fe2cb78f438745eeb211c66f
Vishal Singh <156567984+sangeetanandanvishal04@users.noreply.github.com>
2025-07-07 14:33:45 +0530

    Time: 24 ms (5.52%), Space: 95.1 MB (59.81%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,60 +4,37 @@ public:
         int n1 = nums1.size();
         int n2 = nums2.size(); 
         
-        int i = 0, j = 0, cnt = 0;
-        int n = n1+n2;
-        int idx1 = n / 2;
-        int idx2 = idx1 - 1;
-        int val1 = -1, val2 = -1;
-        
-        while(i<n1 && j<n2){ 
-            if(nums1[i] <= nums2[j]){ 
-                if(cnt == idx1){
-                    val1 = nums1[i];
-                }
-                if(cnt == idx2){
-                    val2 = nums1[i];
-                }
-                cnt++;
-                i++;
-            } 
-            else{ 
-                if(cnt == idx1){
-                    val1 = nums2[j];
-                }
-                if(cnt == idx2){
-                    val2 = nums2[j];
-                }
-                cnt++;
-                j++; 
+        if(n1 > n2){
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int low = 0, high = n1;
+        int n = n1 + n2;
+        int left = (n+1) / 2;
+        while(low <= high){
+            int mid1 = (low+high) / 2;
+            int mid2 = left - mid1;
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+
+            if(mid1-1 >= 0) l1 = nums1[mid1-1];
+            if(mid2-1 >= 0) l2 = nums2[mid2-1];
+
+            if(l1 <= r2 && l2 <= r1){
+                if(n % 2 == 1) return max(l1, l2);
+                return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+            }
+            else if(l1 > r2){
+                high = mid1 - 1;
+            }
+            else{
+                low = mid1 + 1;
             }
         }
 
-        while(i<n1){ 
-            if(cnt == idx1){
-                val1 = nums1[i];
-            }
-            if(cnt == idx2){
-                val2 = nums1[i];
-            }
-            cnt++;
-            i++; 
-        } 
-
-        while(j<n2){ 
-            if(cnt == idx1){
-                val1 = nums2[j];
-            }
-            if(cnt == idx2){
-                val2 = nums2[j];
-            }
-            cnt++;
-            j++; 
-        } 
-        
-        if(n % 2 != 0){ 
-            return val1;
-        }
-        return (val1 + val2) / 2.0;
+        return 0.0;
     }
 };
