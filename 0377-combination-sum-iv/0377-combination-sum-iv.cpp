e034caad2ec732dd6c30c7946235fd604ec74bca
Vishal Singh <156567984+sangeetanandanvishal04@users.noreply.github.com>
2025-07-15 18:33:42 +0530

    Time: 4 ms (30.07%), Space: 9.3 MB (20.37%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -20,7 +20,20 @@ private:
     }  
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        unordered_map<int, int> memo;
-        return solve(target, nums, memo);
+        //unordered_map<int, int> memo;
+        //return solve(target, nums, memo);
+
+        vector<unsigned int> dp(target+1, 0);
+        dp[0] = 1;
+
+        for(int i=1; i<=target; i++){
+            for(int num: nums){
+                if(i-num >= 0){
+                    dp[i] += dp[i-num];
+                }
+            }
+        }
+
+        return dp[target];
     }
 };
