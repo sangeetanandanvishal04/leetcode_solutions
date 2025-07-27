6ff12e95ee6eda5a97a598e72028d4f6758576e6
Vishal Singh <156567984+sangeetanandanvishal04@users.noreply.github.com>
2025-07-27 13:44:19 +0530

    Time: 1372 ms (28.89%), Space: 400.5 MB (32.18%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 private:
     const int MOD = 1e9 + 7;
-    int Solve(int n, int idx, int absent, int late, vector<vector<vector<int>>>& memo){
+    int Solve(int day, int absent, int late, vector<vector<vector<int>>>& memo){
         if(absent == 2 || late == 3){
             return 0;
         }
-        if(idx == n){
+        if(day == 0){
             return 1;
         }
 
-        if(memo[idx][absent][late] != -1){
-            return memo[idx][absent][late];
+        if(memo[day][absent][late] != -1){
+            return memo[day][absent][late];
         }
         
-        int pr = Solve(n, idx+1, absent, 0, memo) % MOD;
-        int abs = Solve(n, idx+1, absent+1, 0, memo) % MOD;
-        int lt = Solve(n, idx+1, absent, late+1, memo) % MOD;
+        int pr = Solve(day-1, absent, 0, memo) % MOD;
+        int abs = Solve(day-1, absent+1, 0, memo) % MOD;
+        int lt = Solve(day-1, absent, late+1, memo) % MOD;
 
-        return memo[idx][absent][late] = ((pr+abs) % MOD + lt) % MOD;
+        return memo[day][absent][late] = ((pr+abs) % MOD + lt) % MOD;
     }    
 public:
     int checkRecord(int n) {
         vector<vector<vector<int>>> memo(n+1, vector<vector<int>>(2, vector<int>(3, -1)));
 
-        return Solve(n, 0, 0, 0, memo);
+        return Solve(n, 0, 0, memo);
     }
 };
