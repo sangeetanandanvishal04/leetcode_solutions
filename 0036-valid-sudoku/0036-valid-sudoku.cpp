67e6960279b9e481cd42a9328d1f4e89bf20b481
Vishal Singh <156567984+sangeetanandanvishal04@users.noreply.github.com>
2025-08-31 21:33:22 +0530

    Time: 0 ms (100%), Space: 21.6 MB (91.87%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -5,25 +5,17 @@ private:
             if(i != row && board[i][col] == ch){
                 return false;
             }
-        }
+            
+            if(i != col && board[row][i] == ch){
+                return false;
+            }
 
-        for(int j=0; j<9; j++){
-            if(j != col && board[row][j] == ch){
+            if((3*(row/3) + i/3) != row && (3*(col/3) + i%3) != col &&  board[3*(row/3) + i/3][3*(col/3) + i%3] == ch){
                 return false;
             }
         }
-
-        int delrow = row-(row%3);
-        int delcol = col-(col%3);
-        for(int i=0; i<3; i++){
-            for(int j=0; j<3; j++){
-                if((i+delrow) != row && (j+delcol) != col && board[i+delrow][j+delcol] == ch){
-                    return false;
-                }
-            }
-        }
         return true;
-    }    
+    } 
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         for(int i=0; i<9; i++){
