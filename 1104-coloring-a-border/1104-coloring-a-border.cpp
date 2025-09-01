39cb443c8703fcaacfe83e3bf64c9c891c2f0ba5
Vishal Singh <156567984+sangeetanandanvishal04@users.noreply.github.com>
2025-09-01 23:13:52 +0530

    Time: 0 ms (100%), Space: 17.6 MB (96.21%) - LeetHub

---
+++ 1104-coloring-a-border/1104-coloring-a-border.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    void dfs(vector<vector<int>>& grid , int i , int j , int target){
+        if(i < 0 || j < 0 || i == grid.size() || j == grid[0].size() || grid[i][j] != target){
+            return;
+        }
+
+        grid[i][j] = -1;
+        dfs(grid , i - 1 , j , target);
+        dfs(grid , i , j - 1 , target);
+        dfs(grid , i + 1 , j , target);
+        dfs(grid , i , j + 1 , target);
+    }
+public:
+    vector<vector<int>> colorBorder(vector<vector<int>>& grid, int row, int col, int color) {
+        vector<vector<int>> res = grid;
+
+        dfs(grid, row, col, grid[row][col]);
+
+        int m = grid.size(), n = grid[0].size();
+
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                if(grid[i][j] < 0){
+                    if(i == 0 || j == 0 || i == m - 1 || j == n - 1){
+                        res[i][j] = color;
+                    }    
+                    else if(grid[i-1][j]>=0 || grid[i][j-1]>=0 || grid[i+1][j]>=0 || grid[i][j+1]>=0){
+                        res[i][j] = color;
+                    }    
+                }
+            }
+        }
+        
+        return res;
+    }
+};
