4c9d10296387ec2f7db39bbd4d8cc9c238aa4b9c
Vishal Singh <156567984+sangeetanandanvishal04@users.noreply.github.com>
2026-01-28 01:04:43 +0530

    Time: 339 ms (42.39%), Space: 316 MB (44.96%) - LeetHub

---
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.cpp
@@ -0,0 +1,50 @@
+class Solution {
+private:
+    int dijkstra(int n, vector<vector<pair<int, int>>>& adj){
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; //{cost, node}
+        vector<int> dist(n, 1e9);
+
+        pq.push({0, 0});
+        dist[0] = 0;
+
+        while(!pq.empty()){
+            int d = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            if(d > dist[node]){
+                continue;
+            }
+
+            for(auto it: adj[node]){
+                int adjNode = it.first;
+                int wt = it.second;
+
+                if(dist[node]+wt < dist[adjNode]){
+                    dist[adjNode] = dist[node] + wt;
+                    pq.push({dist[adjNode], adjNode});
+                }
+            }
+        }
+
+        if(dist[n-1] == 1e9){
+            return -1;
+        }    
+        return dist[n-1];
+    }    
+public:
+    int minCost(int n, vector<vector<int>>& edges) {
+        vector<vector<pair<int, int>>> adj(n);
+
+        for(auto edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+
+            adj[u].push_back({v, wt});
+            adj[v].push_back({u, 2*wt});
+        }
+
+        return dijkstra(n, adj);
+    }
+};
